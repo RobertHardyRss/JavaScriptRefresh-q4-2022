e88987a9c9a67291a830a762c07400652191de9d
mannem1 <mannem1@penguin.lxd>
2022-03-23 16:17:45 -0600

    added code

--- people/emann/scripts/app.js
+++ people/emann/scripts/app.js
@@ -1,1 +1,18 @@
 //@ts-check
+
+
+let myName = 'Eziah ';
+//string  
+let youngerThanYou =true;
+//boolean
+let myAge= 15; 
+//number
+
+let bean = (
+    name: myName,
+    age: myAge,
+);
+//object
+
+let output = document.getElementById('browser-output');
+output.innerText = myName;
