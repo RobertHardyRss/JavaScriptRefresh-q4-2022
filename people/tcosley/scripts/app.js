a0d36c2ee7042c64224f99080b426208a4006507
Thad <cosleytg@s.dcsdk12.org>
2022-03-23 16:12:34 -0600

    chad

--- people/tcosley/scripts/app.js
+++ people/tcosley/scripts/app.js
@@ -1,1 +1,18 @@
 //@ts-check
+let myName = "Thaddy";
+let isBuiltDifferent = true;
+let age = 5436;
+let moneyForMe = {
+    name: myName,
+    age: age,
+    dollers: 34,
+    cents: 23,
+    poor: true,
+};
+
+
+let output = document.getElementById("browser-output");
+
+output.innerText = myName;
+
+
