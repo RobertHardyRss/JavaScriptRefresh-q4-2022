23912ca5abd34c8687f5c8e2245bd660ec6a04bd
monkey-d-luffy <ktkopp@icloud.com>
2022-03-23 16:12:39 -0600

    hi

--- people/kkopp/scripts/app.js
+++ people/kkopp/scripts/app.js
@@ -1,1 +1,15 @@
 //@ts-check
+
+let myName = "kaden";
+let isYoungerThanYou = true;
+let myAge = 14;
+
+let kaden = {
+    name: myName,
+    age: myAge
+};
+
+let output = document.getElementById("browser-output");
+
+output.innerText = myName;
+
