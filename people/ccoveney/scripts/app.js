31d0b148ac782e2eca3223c42432c2fd3f7d8ea9
Caleb-Cove <coveneycn@s.dcsdk12.org>
2022-03-23 16:15:56 -0600

    Commencement

--- people/ccoveney/scripts/app.js
+++ people/ccoveney/scripts/app.js
@@ -1,1 +1,15 @@
 //@ts-check
+
+let Name = "Caleb";
+let dumb = "true";
+let age = 9999990000099999999999999;
+let rules = "Rules Of Nature";
+let memes = {
+	name: Name,
+	dumb: dumb,
+	age: age,
+};
+
+let output = document.getElementById("browser-output");
+
+output.innerText = rules;
