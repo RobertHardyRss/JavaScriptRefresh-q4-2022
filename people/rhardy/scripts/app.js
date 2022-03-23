a4f081786fd38935eb8e2500ae43d280baa833fb
Robert Hardy <rhardy@dcsdk12.org>
2022-03-23 16:15:19 -0600

    Personalized files and added some script

--- people/rhardy/scripts/app.js
+++ people/rhardy/scripts/app.js
@@ -1,1 +1,18 @@
 //@ts-check
+
+// string
+let myName = "Robert";
+// boolean
+let isOlderThanYou = true;
+// numeric
+let myAge = 51;
+
+// object
+let robert = {
+	name: myName,
+	age: myAge,
+};
+
+let output = document.getElementById("browser-output");
+
+output.innerText = myName;
