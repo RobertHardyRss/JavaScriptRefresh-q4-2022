958a94ddcafa9343bf35a2ee2d4fc3905279f1d6
ArtemisOak <thompsonaj4@s.dcsdk12.org>
2022-03-23 16:15:39 -0600

    personalized page

--- people/athohmpson/scripts/app.js
+++ people/athohmpson/scripts/app.js
@@ -1,1 +1,10 @@
 //@ts-check
+let myName = "Artemis";
+let isFunny = true;
+let myAge = 16;
+let cat = {
+	name: myName,
+	age: myAge,
+};
+let output = document.getElementById("browser-output");
+output.innerText = myName;
