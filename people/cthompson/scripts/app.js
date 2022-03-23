b687b0bb3947263fdd33a030a567aee16f0004ed
Christopher <majorhayes0@gmail.com>
2022-03-23 15:16:45 -0700

    add initialize function

--- people/cthompson/scripts/app.js
+++ people/cthompson/scripts/app.js
@@ -1,1 +1,15 @@
-//@ts-check
+// @ts-check
+const name = "Chris";
+const age = 42;
+const awesome = true;
+
+export const person = {
+	name,
+	age,
+	awesome,
+};
+
+export function initialize() {
+	const output = document.getElementById("output");
+	output.innerHTML = `Hello <b>${person.name}</b>!`;
+}
