7460235a6539970f773f3cddf00d6b5450ce08fc
Kaden Wood <altairwood2@gmail.com>
2022-03-23 16:16:17 -0600

    Personalized Page

--- people/kwood/scripts/app.js
+++ people/kwood/scripts/app.js
@@ -1,1 +1,22 @@
-//@ts-check
+//string
+let myName = "Kaden Wood";
+//boolean
+let isOld = false;
+//numeric
+let age = 14;
+//object
+let myObject = {
+    name: myName,
+    age: age,
+    old: isOld
+};
+
+let output = document.getElementById('browser-output');
+
+output.innerText = myName;
+
+
+
+
+
+
