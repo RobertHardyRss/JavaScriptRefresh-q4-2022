c2bd86aa93965e61d9a45b7f2424104fbca7054f
Christopher <majorhayes0@gmail.com>
2022-03-23 15:28:26 -0700

    add Person object

--- people/cthompson/scripts/app.js
+++ people/cthompson/scripts/app.js
@@ -3,13 +3,30 @@ const name = "Chris";
 const age = 42;
 const awesome = true;
 
-export const person = {
+class Person {
+	constructor({ name, age, children, awesome }) {
+		this.name = name;
+		this.age = age;
+		this.children = children;
+		this.awesome = awesome;
+	}
+}
+
+const zach = new Person({
+	name: "Zach",
+	age: 14,
+	children: [],
+	awesome: false,
+});
+export const chris = new Person({
 	name,
 	age,
 	awesome,
-};
+	children: [zach],
+});
 
 export function initialize() {
 	const output = document.getElementById("output");
-	output.innerHTML = `Hello <b>${person.name}</b>!`;
+	const children = chris.children.map((c) => c.name).join(", ");
+	output.innerHTML = `Hello <b>${chris.name}.</b>! Children: <b>${children}</b>`;
 }
