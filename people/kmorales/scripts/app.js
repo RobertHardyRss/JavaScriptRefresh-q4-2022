c8198aa3042979f45726b0451f09e36d30dfddfc
Kaylee Morales <thisiskaywees@gmail.com>
2022-03-23 15:16:48 -0600

    Added script

--- people/kmorales/scripts/app.js
+++ people/kmorales/scripts/app.js
@@ -1,1 +1,20 @@
 //@ts-check
+
+//let is better than var. Const is constant. Camel casing is like myName/isOlderThanYou. The orange thing is called a string.
+//The older than you is a boolien, only true or false. The myAge variable is a number. Objects(4th variable) are surrounded by
+//curly braces. They can be empty or have properties.Objects can have other variables within.
+
+let myName = "Kaywee";
+
+let isOlderThanYou = false;
+
+let myAge = 16;
+
+let Kaywee = {
+	name: myName,
+	age: myAge,
+};
+//Document.getelement will reference the html, and allow you to write data to it.
+let outPut = document.getElementById('"browser-output"');
+
+outPut.innerText = myName;
