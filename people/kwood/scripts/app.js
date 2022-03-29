6b4e4f4f91dde6d189fb5e0fab5538ccd13c8617
Kaden Wood <altairwood2@gmail.com>
2022-03-29 15:42:24 -0600

    Made a function on click

--- people/kwood/scripts/app.js
+++ people/kwood/scripts/app.js
@@ -6,17 +6,52 @@ let isOld = false;
 let age = 14;
 //object
 let myObject = {
-    name: myName,
-    age: age,
-    old: isOld
+	name: myName,
+	age: age,
+	old: isOld,
 };
 
-let output = document.getElementById('browser-output');
+let output = document.getElementById("browser-output");
 
 output.innerText = myName;
 
+let isItFalsy = [
+	false,
+	0,
+	-0,
+	"",
+	[],
+	{},
+	() => {},
+	function () {},
+	null,
+	undefined,
+	true,
+	"False",
+	"0",
+	"-0",
+];
 
+isItFalsy.forEach((item) => {
+	let outputString = `\n${typeof item} ${item}`;
 
+	outputString += item ? " is truthy" : " is falsy";
+	output.innerText += outputString;
 
+	//if(! item){
+	//output.append(`\n${item} is falsy `);
+	//}
+	//else{
+	//output.append(`\n${item} is truthy `);
+	//}
+});
+
+let trueValues = isItFalsy.filter((x) => x);
+let falseValues = isItFalsy.filter((x) => !x);
+console.log(trueValues, falseValues);
+
+document.addEventListener("click", (e) => {
+    output.innerText = `x: ${e.x} y: ${e.y}`
+});
 
 
