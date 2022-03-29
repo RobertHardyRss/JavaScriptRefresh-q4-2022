6229b8e08572a31d0389a5cbe9022305736ee10c
ArtemisOak <thompsonaj4@s.dcsdk12.org>
2022-03-29 15:44:14 -0600

    FIREworks

--- people/athohmpson/scripts/app.js
+++ people/athohmpson/scripts/app.js
@@ -8,3 +8,32 @@ let cat = {
 };
 let output = document.getElementById("browser-output");
 output.innerText = myName;
+
+let isItFalsey = [
+	false,
+	0,
+	-0,
+	"",
+	[],
+	{},
+	() => {},
+	function () {},
+	undefined,
+	null,
+	true,
+	"false",
+	"0",
+];
+
+isItFalsey.forEach((item) => {
+	let outputString = `\n${typeof item} ${item}`;
+	outputString += item ? " is truthy" : " is falsey";
+	output.innerText += outputString;
+});
+
+let trueValues = isItFalsey.filter((x) => x);
+console.log(trueValues);
+
+document.addEventListener("click", (e) => {
+	output.innerText = `x: ${e.x} y: ${e.y}`;
+});
