c1f4a39ef13664d61a18180ce7be1f0a5de922cc
monkey-d-luffy <ktkopp@icloud.com>
2022-03-29 15:43:21 -0600

    change

--- people/kkopp/scripts/app.js
+++ people/kkopp/scripts/app.js
@@ -1,15 +1,49 @@
 //@ts-check
 
-let myName = "kaden";
-let isYoungerThanYou = true;
+// string
+let myName = "Kaden";
+// boolean
+let isOlderThanYou = true;
+// numeric
 let myAge = 14;
 
+// object
 let kaden = {
-    name: myName,
-    age: myAge
+	name: myName,
+	age: myAge,
 };
 
 let output = document.getElementById("browser-output");
 
 output.innerText = myName;
 
+let isItFalsy = [
+	false,
+	0,
+	-0,
+	"",
+	[],
+	{},
+	() => {},
+	function () {},
+	undefined,
+	null,
+	true,
+	"false",
+	"0",
+];
+
+isItFalsy.forEach((item) => {
+	// this will tell us the type of item and its value
+	let outputString = `\n${typeof item} ${item} `;
+	outputString += item ? " is truthy" : " is falsy";
+	output.innerText += outputString;
+});
+
+let trueValues = isItFalsy.filter((x) => x);
+let falseValues = isItFalsy.filter((x) => !x);
+console.log("True Values", trueValues, "False Values", falseValues);
+
+document.addEventListener("click", (e) => {
+	output.innerText = `x: ${e.x} y: ${e.y}`;
+});
