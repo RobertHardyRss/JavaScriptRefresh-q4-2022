1ebac5960d8ee345f06a6e8cc4dad7c3185455b1
Caleb-Cove <coveneycn@s.dcsdk12.org>
2022-03-29 15:48:44 -0600

    Upload

--- people/ccoveney/scripts/app.js
+++ people/ccoveney/scripts/app.js
@@ -3,7 +3,7 @@
 let Name = "Caleb";
 let dumb = "true";
 let age = 9999990000099999999999999;
-let rules = "Rules Of Nature";
+let rules = "Sus amogus";
 let memes = {
 	name: Name,
 	dumb: dumb,
@@ -13,3 +13,33 @@ let memes = {
 let output = document.getElementById("browser-output");
 
 output.innerText = rules;
+
+let isItFalse = [
+	false,
+	0,
+	-0,
+	"",
+	[],
+	{},
+	() => {},
+	function () {},
+	undefined,
+	null,
+	//truth
+	true,
+	"false",
+	"0",
+];
+
+isItFalse.forEach((item) => {
+	let outputString = `\n${typeof item} ${item}`;
+	outputString += item ? "is truthy" : "is falsy";
+	output.innerText += outputString;
+});
+
+let trueValues = isItFalse.filter((x) => x);
+let falseValues = isItFalse.filter((x) => !x);
+
+document.addEventListener("click", (e) => {
+	output.innerText = `x: ${e.x} y: ${e.y}`;
+});
