5c9e64617fedd8116a720f90a1b96ad18e21083e
Tanner <TannerEX325@gmail.com>
2022-04-01 16:15:20 -0600

    Commit1

--- people/tarro/scripts/app.js
+++ people/tarro/scripts/app.js
@@ -1,1 +1,33 @@
 //@ts-check
+let myName = "Tanner";
+let age = 15;
+let isItFalsy = [
+    false,
+    0,
+    -0,
+    "",
+    [],
+    {},
+    {} == {},
+    function () {},
+    undefined,
+    null,
+    "false",
+    "0",
+];
+let output = document.getElementById("browser-output");
+
+isItFalsy.forEach((item) => {
+    // boolean false
+    return
+    let outputString = `${typeof item } ${item} `;
+    outputString += item ? " is truthy" : " is falsey";
+    output.innerText += outputString;
+    "false"
+});
+
+document.addEventListener("click", (e) => {
+    output.innerText = `x: ${e.x} ${e.y}`
+    console.log(`Function "click" passed. x: ${e.x} y: ${e.y}`)
+    
+});
---
+++ people/tarro/scripts/canvas.js
@@ -0,0 +1,9 @@
+//@ts-check
+
+/** @type {HTMLCanvasElement} */
+//@ts-ignore
+ let canvas = document.getElementById("Canvas0.0.1");
+ let ctx = canvas.getContext("2d");
+
+ canvas.width = window.innerWidth;
+ canvas.height = window.innerHeight;
